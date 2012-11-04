b27610c36745bfe4e3f863c6fa9e1bfde8047f4a
fritz <fritz.groes@gmail.com>
2012-11-04 21:59:22 +0100

    kalman filter implemented as class, not correctly working

--- kalman/main.py
+++ kalman/main.py
@@ -3,11 +3,71 @@ import matplotlib.pyplot as plt
 
 
 def initPlot(N):
-	plt.ion()
 	fig = plt.figure()
 	ax = fig.add_subplot(1, 1, 1)
-	ax.axis([0, N, 0, 1000])
+	ax.axis([0, N, 0, 500])
 	return ax
+
+
+class filterKalman(object):
+
+	def __init__(self):
+		self.optimal = False
+	
+	def setModelStateTransition(self, modelStateTransition):
+		self.F = np.matrix(modelStateTransition)
+
+	def setModelControlInput(self, modelControlInput):
+		self.G = np.matrix(modelControlInput)
+
+	def setParameterControlInput(self, parameterControlInput):
+		self.u = np.matrix(parameterControlInput)
+
+	def setNoiseProcess(self, noiseProcess):
+		self.Q = np.matrix(noiseProcess)
+
+	def setModelObservation(self, modelObservation):
+		self.H = np.matrix(modelObservation)
+
+	def setNoiseObservation(self, noiseObservation):
+		self.R = np.matrix(noiseObservation)
+
+	def setState(self, state, covariance=None):
+		self.x = np.matrix(state)
+		N = len(self.x)
+		if covariance == None:
+			self.P = np.matrix(zeros((N, N)))
+		else:
+			self.P = np.matrix(covariance)
+		self.E = np.matrix(np.diag(np.ones(N)))
+
+	def __predict(self):
+		self.x = self.F * self.x + self.G * self.u
+		self.P = self.F * self.P * self.F.T + self.Q
+
+	def setOptimalKalmanGain(self, optimal):
+		self.optimal = optimal
+
+	def __update(self):
+		K = self.P * self.H.T * (self.H * self.P * self.H.T + self.R).I
+		e = self.z - self.H * self.x
+		self.x = self.x + K * e
+		if self.optimal == True:
+			self.P = (self.E - K * self.H) * self.P
+		else:
+			KH = self.E - K * self.H
+			self.P = KH * self.P * KH.T + K * self.R * K.T
+
+	def setObservation(self, observation):
+		self.z = observation
+
+	def advance(self, observation):
+		self.z = observation 
+		self.__predict()
+		self.__update()
+
+	def getState(self):
+		return self.x
 
 
 if __name__ == '__main__':
@@ -15,32 +75,27 @@ if __name__ == '__main__':
 	v0 = 20 # m s-1
 	a0 = 2 # m s-2
 	sx = 5 # m s-1
-	sxm = 20 # m s-1
+	sxm = 4 # m s-1
 	sv = 1 # m s-2
 	sa = 0.1 # m s-1
 	dt = 0.1 # s
 	Nm = 100
+	kalman = filterKalman()
+	kalman.setModelStateTransition([[1, dt], [0, dt]])
+	kalman.setModelControlInput([[dt ** 2 / 2], [dt]])
+	kalman.setParameterControlInput(a0)
+	kalman.setNoiseProcess(sa * np.array([[dt ** 4 / 4, dt ** 3 / 2], [dt ** 3 / 2, dt ** 2]]))
+	kalman.setModelObservation([1, 0])
+	kalman.setNoiseObservation([sxm])
+	kalman.setState([[x0], [v0]], [[sx, 0], [0, sv]])
 	ex = np.random.normal(0, sxm, Nm)
-	ev = np.random.normal(0, sv, Nm)
-	xPre = np.matrix([[x0], [v0]])
-	PPre = np.matrix([[sx, 0], [0, sv]])
-	F = np.matrix([[1, dt], [0, dt]])
-	G = np.matrix([[dt ** 2 / 2], [dt]])
-	Q = np.matrix([[dt ** 4 / 4, dt ** 3 / 2], [dt ** 3 / 2, dt ** 2]]) * sa
-	H = np.matrix([1, 0])
-	R = np.matrix([sxm])
-	z = x0
+	z = np.array([x0 + v0 * (i + 1) * dt + a0 * ((i + 1) * dt) ** 2 / 2 + ex[i] for i in range(Nm)])
 	ax = initPlot(Nm)
-	x = xPre
-	P = PPre
+	xx = np.zeros((2, Nm))
+	#kalman.setOptimalKalmanGain(True)
 	for i in range(Nm):   
-		z = x0 + v0 * (i + 1) * dt + a0 * ((i + 1) * dt) ** 2 / 2 + ex[i]
-		x = F * x + G * a0
-		P = F * P * F.T + Q
-		K = P * H.T * (H * P * H.T + R).I
-		e = z - H * x
-		x = x + K * e
-		ax.plot(i, z, 'ro')
-		ax.plot(i, x[0], 'ko', ms=2)
-		plt.draw()
+		kalman.advance(z[i])
+		xx[:, i] = kalman.getState().T
+	ax.plot(z, 'ko')
+	ax.plot(xx[0, :], 'r-', lw=2)
 	plt.show()
